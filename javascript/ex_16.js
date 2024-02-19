9cdbd0117837972272d8df1f23b3e02bfdb79975
AmrElshafii <amrabdalaala@gmail.com>
2024-02-19 10:21:33 +0200

    k

--- javascript/ex_16.js
+++ javascript/ex_16.js
@@ -130,3 +130,24 @@
 // 33
 // "Available"
 // "Laravel"
+
+/*
+const question = new Map([
+    ['question', 'What is the best programming language in the world?'],
+    [1, 'C'],
+    [2, 'Java'],
+    [3, 'JavaScript'],
+    ['correct', 3],
+    [true, 'Correct 🎉'],
+    [false, 'Try again!'],
+]);
+// console.log(question);
+
+// Convert object to map
+
+// Convert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log(...question.keys());
+console.log(...question.values());
+*/
